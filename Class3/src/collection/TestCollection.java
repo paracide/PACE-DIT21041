4f582666169d56688ff41f2c9ec13e48d8ca22c6
Arthur <paracidex@gmail.com>
2024-04-19 11:25:36 -0500

    feat(class3): addElements using type parameter

--- Class3/src/collection/TestCollection.java
+++ Class3/src/collection/TestCollection.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class TestCollection {
   public static void main(String[] args) {
     // Array
-    int[] numbers = {512, 3, 1, 2, 4};
+    Integer[] numbers = {512, 3, 1, 2, 4};
     Arrays.sort(numbers);
     System.out.println(Arrays.toString(numbers));
 
@@ -21,15 +21,9 @@ public class TestCollection {
     System.out.println(Arrays.toString(fruits));
   }
 
-  public static int[] addElements(int[] arr, int addNum) {
-    int[] result = Arrays.copyOf(arr, arr.length + 1);
-    result[result.length - 1] = addNum;
-    return result;
-  }
-
-  public static String[] addElements(String[] fruits, String fruit) {
-    String[] result = Arrays.copyOf(fruits, fruits.length + 1);
-    result[result.length - 1] = fruit;
+  public static <T> T[] addElements(T[] arr, T newElement) {
+    T[] result = Arrays.copyOf(arr, arr.length + 1);
+    result[result.length - 1] = newElement;
     return result;
   }
 }
